0b26488d7ea52d1ae84f923e68b16013507c6d99
Bayer <kmtsm1692004@gmail>
2026-03-08 02:35:31 +0300

    solve the scroll in start page

--- src/ui/pages/StartPage.jsx
+++ src/ui/pages/StartPage.jsx
@@ -7,8 +7,8 @@ export default function StartPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-full flex flex-col gap-10 p-10">
-      <div className="flex flex-col justify-center items-center gap-5 font-[LobsterTwo] grow">
+    <div className="min-h-full flex flex-col justify-center gap-25 p-10">
+      <div className="flex flex-col items-center gap-5 font-[LobsterTwo]">
         <h1 className="text-8xl ">Reporter</h1>
         <p>Write your report with easy way</p>
         <CustomButton
